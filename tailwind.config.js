7781a1e044d1bbb0a39e7b44280ad772efd62b82
mirza assidiqie <assidiqiemirza@gmail.com>
2023-02-10 20:19:30 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['ket.html', 'tesuto.html'],
+  theme: { 
+    extend: {
+      fontFamily: {
+        'headerf': ["Keep Calm"] ,
+        'main' : ["A apple tea"],
+        'sec' : ["aAssassinNinja"],
+        'coco' : ['Cocogoose'],
+      },
+      colors: {
+        primary: '#3F8080',
+        secondary: '#F57BA4',
+        tertiary: '#F7CB53',
+      }
+  
+    },
+  },
+  plugins: [
+    
+  ],
+}
